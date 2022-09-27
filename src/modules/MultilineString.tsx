a75f9f825ddee44916c2466f4c3adb1f76171e6c
polycloud-dev <heidenreichdd@gmail.com>
2022-09-27 20:11:49 +0200

    fixed new line at end

--- src/modules/MultilineString.tsx
+++ src/modules/MultilineString.tsx
@@ -1,12 +1,17 @@
+import React from "react";
+
 export default function MultilineString({ string }: { string: string }) {
     const lines = string.split('\n');
     return (
         <>
-            {lines.map(line => (
-                <>
+            {lines.map((line, index) => (
+                <React.Fragment key={Math.random() + ''}>
                     {line}
-                    <br />
-                </>
+                    {
+                        index !== lines.length - 1 &&
+                        <br />
+                    }
+                </React.Fragment>
             ))}
         </>
     );

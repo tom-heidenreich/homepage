ce1d51345554d74b9b07d240cceab7684ba98d4d
polycloud-dev <heidenreichdd@gmail.com>
2022-09-27 20:57:59 +0200

    added icon wrapper

---
+++ src/components/icons/Icons.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as IconGolang } from './golang.svg';
+import { ReactComponent as IconReact } from './react.svg';
+import { ReactComponent as IconTypescript } from './typescript.svg';
+import { ReactComponent as IconJavascript } from './javascript.svg';
+import { ReactComponent as IconPython } from './python.svg';
+import { ReactComponent as IconJava } from './java.svg';
+import { ReactComponent as IconTensorflow } from './tensorflow.svg';
+import { ReactComponent as IconNext } from './nextjs.svg';
+import { ReactComponent as IconDocker } from './docker.svg';
+
+const icons = {
+    Golang: IconGolang,
+    React: IconReact,
+    Typescript: IconTypescript,
+    Javascript: IconJavascript,
+    Python: IconPython,
+    Java: IconJava,
+    Tensorflow: IconTensorflow,
+    Next: IconNext,
+    Docker: IconDocker,
+}
+
+export default icons;
